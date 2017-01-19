2071e318bcd0ee80431923fd57583460f9fef742
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2017-01-19 20:26:31 +0100

    Initial files

---
+++ pack.pl
@@ -0,0 +1,6 @@
+name(mime).
+title('MIME processing library based on maildrop').
+version('0.5.0').
+keywords([mime, maildrop]).
+author('Jan Wielemaker', 'J.Wielemaker@vu.nl').
+download('https://github.com/JanWielemaker/mime/archive/V0.5.0.tar.gz').
